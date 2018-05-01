cc691905b9fd215a8aa8d13ea4598e87a31a38db
LUCIANO MARINHO <nickmarinho@gmail.com>
2018-05-01 00:55:12 -0300

    Adjusts in the authenticate method in loops to find users. Adjusts in message logs.

--- routes/users.js
+++ routes/users.js
@@ -52,7 +52,7 @@ router.put('/:userId', function(req, res) {
   var usersData = fs.readFileSync(usersDbFile, 'utf8') ? JSON.parse(fs.readFileSync(usersDbFile, 'utf8')) : [];
   var userId = req.params.userId;
 
-  for (var d = 1, len = usersData.length; d < len; d += 1) {
+  for (var d = 0, len = usersData.length; d < len; d += 1) {
     if (userId.indexOf(usersData[d].id) !== -1) {
       usersData.splice(d, 1);
       req.body.password = bcrypt.hashSync(req.body.password);
@@ -68,33 +68,43 @@ router.put('/:userId', function(req, res) {
   console.log('', msg);
 });
 
-router.post('/authenticate', function(req, res) {
+router.post('/authenticate', function(req, res, next) {
   var usersData = fs.readFileSync(usersDbFile, 'utf8') ? JSON.parse(fs.readFileSync(usersDbFile, 'utf8')) : [];
+  var userFound = 0;
 
   for (var d = 0, len = usersData.length; d < len; d += 1) {
-    if (toString(usersData[d].email) === toString(req.body.email)) {
+    if (req.body.email.indexOf(usersData[d].email) !== -1) {
       if(bcrypt.compareSync(req.body.password, usersData[d].password)) {
-        res.status(200).send({ auth: true });
+        res.status(200).send({ auth: true, email: usersData[d].email, role: usersData[d].role });
+        console.log('User logged: ' + JSON.stringify(usersData[d].email));
         var msg = log.showDate();
         console.log('', msg);
+        userFound++;
+        return;
       } else {
-        res.status(401).send('Failed to authenticate: ' + toString(usersData[d].email));
+        var errMsg = 'Failed to authenticate: ' + req.body.email;
+        res.status(401).send(errMsg);
+        console.log(errMsg);
         var msg = log.showDate();
         console.log('', msg);
+        return;
       } 
-    } else {
-      res.status(404).send('No user found: ' + toString(usersData[d].email));
-      var msg = log.showDate();
-      console.log('', msg);
-    }  
+    }
   }
+  
+  if (!userFound) {
+    res.status(404).send('No user found: ' + toString(usersData[d].email));
+    var msg = log.showDate();
+    console.log('', msg);
+    return;
+  }  
 });
 
 router.delete('/:userId', function(req, res) {
   var usersData = fs.readFileSync(usersDbFile, 'utf8') ? JSON.parse(fs.readFileSync(usersDbFile, 'utf8')) : [];
   var userId = req.params.userId;
 
-  for (var d = 1, len = usersData.length; d < len; d += 1) {
+  for (var d = 0, len = usersData.length; d < len; d += 1) {
     if (userId.indexOf(usersData[d].id) !== -1) {
       usersData.splice(d, 1);
       fs.writeFileSync(usersDbFile, JSON.stringify(usersData) , 'utf-8');
